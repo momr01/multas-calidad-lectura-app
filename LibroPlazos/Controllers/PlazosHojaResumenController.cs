8ad296b6ee3196fad53feec2995548a8e530524f
Maximiliano Montaña <maxi.omr01@gmail.com>
2024-07-18 18:06:19 -0300

    se continua creando funciones para libro plazos

--- LibroPlazos/Controllers/PlazosHojaResumenController.cs
+++ LibroPlazos/Controllers/PlazosHojaResumenController.cs
@@ -11,81 +11,239 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace MultasLectura.LibroPlazos.Controllers
 {
+    public class Generics
+    {
+        public object Value { get; set; }
+
+        public Generics(object value)
+        {
+            Value = value;
+        }
+
+        public T GetValue<T>()
+        {
+            return (T)Value;
+        }
+    }
+
+
     public class PlazosHojaResumenController : IPlazosHojaResumenController
     {
+        private readonly BaremoModel _baremos;
+
+        public PlazosHojaResumenController(BaremoModel baremos)
+        {
+            _baremos = baremos;
+        }
+
         public void CrearTablaDatosPorTarifa(ExcelWorksheet hojaResumen, ExcelWorksheet hojaReclDetalles)
+        {
+            List<TarifaPlazosModel> tarifas = new()
+            {
+                new("t1", "itinerario t1", "días hábiles t1", 'a', _baremos.T1),
+                new("t2", "itinerario  t2", "días hábiles t2", 'f', _baremos.T2),
+                new("t3", "itinerario  t3", "días hábiles t3", 'k', _baremos.T3),
+                new("altura t1", "altura t1", "días hábiles altura t1", 'p', _baremos.AlturaT1),
+                new("altura t3", "altura t3", "días hábiles altura t3", 'u', _baremos.AlturaT3)
+            };
+
+            foreach(TarifaPlazosModel tarifa in tarifas)
+            {
+                CrearTablaDatosPorTarifa(hojaResumen, hojaReclDetalles, tarifa);
+            }
+           
+
+
+
+        }
+
+        private char ObtenerLetraSiguiente(char letra)
+        {
+            // Verificar si la letra es una letra del alfabeto
+           // if (!char.IsLetter(letra))
+           // {
+          //      throw new ArgumentException("El carácter proporcionado no es una letra.");
+          //  }
+
+            // Obtener el código ASCII de la letra
+            int asciiCode = (int)letra;
+
+            // Incrementar el código ASCII y ajustar para el caso de 'z' y 'Z'
+            if (letra == 'z')
+            {
+                asciiCode = (int)'a';
+            }
+            else if (letra == 'Z')
+            {
+                asciiCode = (int)'A';
+            }
+            else
+            {
+                asciiCode++;
+            }
+
+            // Convertir de nuevo a carácter
+            return (char)asciiCode;
+        }
+
+        private void CrearTablaDatosPorTarifa(ExcelWorksheet hojaResumen, ExcelWorksheet hojaReclDetalles, TarifaPlazosModel tarifa )
         {
             int numFilaInicial = 3;
             int numFila = 3;
 
-            hojaResumen.Cells[$"A1"].Value = "DIAS HÁBILES T1";
+            string primLetra = tarifa.LetraInicial.ToString().ToUpper();
+            char letra2 = ObtenerLetraSiguiente(tarifa.LetraInicial);
+            string segLetra = letra2.ToString().ToUpper();
+            char letra3 = ObtenerLetraSiguiente(letra2);
+            string tercLetra = letra3.ToString().ToUpper();
+            char letra4 = ObtenerLetraSiguiente(letra3);
+            string cuarLetra = letra4.ToString().ToUpper();
 
-            hojaResumen.Cells["A1:C1"].Merge = true;
-            hojaResumen.Cells[$"A2"].Value = "FTL";
-            hojaResumen.Cells[$"B2"].Value = "k";
-            hojaResumen.Cells[$"C2"].Value = "Qij";
+            hojaResumen.Cells[$"{primLetra}1"].Value = tarifa.Encabezado.ToUpper();
 
-            List<int> ftl = new()
+            hojaResumen.Cells[$"{primLetra}1:{cuarLetra}1"].Merge = true;
+            hojaResumen.Cells[$"{primLetra}2"].Value = "FTL";
+            
+            hojaResumen.Cells[$"{segLetra}2"].Value = "k";
+            LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{primLetra}2:{segLetra}2"], Color.FromArgb(1, 204, 255, 255 ));
+            hojaResumen.Cells[$"{tercLetra}2"].Value = "Qij";
+            hojaResumen.Cells[$"{tercLetra}2:{cuarLetra}2"].Merge = true;
+            LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{tercLetra}2"], Color.FromArgb(1, 255, 0, 0));
+
+            LibroExcelHelper.AplicarBordeGruesoARango(hojaResumen.Cells[$"{primLetra}1:{cuarLetra}2"]);
+
+            List<int> ftlLista = new()
           ;
 
-            List<int> k = new();
+           
 
-            for(int i = -14; i < 18; i++)
+            for (int i = -14; i < 18; i++)
             {
-                ftl.Add(i);
+                ftlLista.Add(i);
 
             }
 
-            foreach(int num in ftl)
+          
+
+
+
+            foreach (int ftl in ftlLista)
             {
-                hojaResumen.Cells[$"A{numFila}"].Value = num;
+                hojaResumen.Cells[$"{primLetra}{numFila}"].Value = ftl;
 
-                int cantidad = CalcularCantAtrasos(hojaReclDetalles, num);
+                int cantidad = CalcularCantAtrasos(hojaReclDetalles, ftl, tarifa.Descripcion);
 
-                hojaResumen.Cells[$"C{numFila}"].Value = cantidad;
+                hojaResumen.Cells[$"{tercLetra}{numFila}"].Value = cantidad;
 
-                ColorearRangoSegunNum(num, hojaResumen, numFila);
+                ColorearRangoSegunNum(ftl, hojaResumen, numFila, primLetra, tercLetra);
 
-                if(numFila == numFilaInicial)
-                {
-                    hojaResumen.Cells[$"A{numFila}"].Style.Border.Top.Style = ExcelBorderStyle.Thick;
-                    hojaResumen.Cells[$"A{numFila}"].Style.Border.Left.Style = ExcelBorderStyle.Thick;
-                    hojaResumen.Cells[$"A{numFila}"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+                AplicarBordeARango(numFila, numFilaInicial, ftlLista, hojaResumen, tarifa.LetraInicial);
+                AplicarBordeARango(numFila, numFilaInicial, ftlLista, hojaResumen, letra2);
+                AplicarBordeARango(numFila, numFilaInicial, ftlLista, hojaResumen, letra3);
 
-                }
+                CargarDatosColK(ftl, hojaResumen, numFila, segLetra);
+
+                hojaResumen.Cells[$"{tercLetra}{numFila}:{cuarLetra}{numFila}"].Merge = true;
+
+
 
 
                 numFila++;
 
             }
 
-          // LibroExcelHelper.AplicarBordeGruesoARango(hojaResumen.Cells[$"A{numFilaInicial}:A{numFila}"]);
+            // LibroExcelHelper.AplicarBordeGruesoARango(hojaResumen.Cells[$"A{numFilaInicial}:A{numFila}"]);
+            hojaResumen.Cells[$"{primLetra}1:{cuarLetra}{numFila - 1}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            hojaResumen.Cells[$"{primLetra}1:{cuarLetra}{numFila - 1}"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+            /* List<string> headersTabla2 = new()
+             {
+                 "Certificado", "Valor de Lectura", "Dentro Plazo", "Bonificación"
+             };*/
+
+            List<double> dentroPlazo = new() { 41, 105000 };
+
+            Dictionary<string, Generics> valoresTabla2 = new();
+            valoresTabla2.Add("Certificado", new Generics(78000000));
+            valoresTabla2.Add("Valor de Lectura", new Generics(tarifa.Baremos));
+            valoresTabla2.Add("Dentro Plazo", new Generics(dentroPlazo));
+            valoresTabla2.Add("Bonificación", new Generics(566));
 
 
+            for (int  i = 0; i < 4; i++)
+            {
+               // hojaResumen.Cells[$"{primLetra}{numFila}"].Value = headersTabla2[i];
+                hojaResumen.Cells[$"{primLetra}{numFila}:{segLetra}{numFila}"].Merge = true;
+                numFila++;
+            }
 
+          
 
         }
 
-        private void ColorearRangoSegunNum(int num, ExcelWorksheet hojaResumen, int numFilaInicial)
+        private void CargarDatosColK(int ftl, ExcelWorksheet hojaResumen, int numFila, string letra)
         {
-            if (num <= -6 || num >= 4)
+            if (ftl <= -3)
             {
-                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"A{numFilaInicial}:C{numFilaInicial}"], Color.FromArgb(1, 255, 102, 0));
+                hojaResumen.Cells[$"{letra}{numFila}"].Value = (ftl + 3) * -1;
             }
-            else if (num == -5 || num == 3)
+            else if (ftl >= 2)
             {
-                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"A{numFilaInicial}:C{numFilaInicial}"], Color.FromArgb(1, 255, 204, 153));
-            }
-            else if (num == -4 || num == 2)
-            {
-                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"A{numFilaInicial}:C{numFilaInicial}"], Color.FromArgb(1, 255, 255, 153));
+                hojaResumen.Cells[$"{letra}{numFila}"].Value = ftl - 1;
             }
             else
             {
-                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"A{numFilaInicial}:C{numFilaInicial}"], Color.FromArgb(1, 204, 255, 204));
+                hojaResumen.Cells[$"{letra}{numFila}"].Value = 0;
+            }
+        }
+
+        private void AplicarBordeARango(int numFila, int numFilaInicial, List<int> ftl, ExcelWorksheet hojaResumen, char letraCelda)
+        {
+            string letra = letraCelda.ToString().ToUpper();
+
+            if (numFila == numFilaInicial)
+            {
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Top.Style = ExcelBorderStyle.Thick;
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Left.Style = ExcelBorderStyle.Thick;
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+
+            }
+            else if (numFila == (ftl.Count + numFilaInicial - 1))
+            {
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Bottom.Style = ExcelBorderStyle.Thick;
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Left.Style = ExcelBorderStyle.Thick;
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+
+            }
+            else
+            {
+
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Left.Style = ExcelBorderStyle.Thick;
+                hojaResumen.Cells[$"{letra}{numFila}"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+            }
+        }
+
+        private void ColorearRangoSegunNum(int num, ExcelWorksheet hojaResumen, int numFilaInicial, string letra1, string letra3)
+        {
+            if (num <= -6 || num >= 4)
+            {
+                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{letra1}{numFilaInicial}:{letra3}{numFilaInicial}"], Color.FromArgb(1, 255, 102, 0));
+            }
+            else if (num == -5 || num == 3)
+            {
+                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{letra1}{numFilaInicial}:{letra3}{numFilaInicial}"], Color.FromArgb(1, 255, 204, 153));
+            }
+            else if (num == -4 || num == 2)
+            {
+                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{letra1}{numFilaInicial}:{letra3}{numFilaInicial}"], Color.FromArgb(1, 255, 255, 153));
+            }
+            else
+            {
+                LibroExcelHelper.FondoSolido(hojaResumen.Cells[$"{letra1}{numFilaInicial}:{letra3}{numFilaInicial}"], Color.FromArgb(1, 204, 255, 204));
             }
             /* List<int> ftl = new()
           ;
@@ -111,7 +269,7 @@ namespace MultasLectura.LibroPlazos.Controllers
 
 
 
-        private int CalcularCantAtrasos(ExcelWorksheet hojaBase, int atraso)
+        private int CalcularCantAtrasos(ExcelWorksheet hojaBase, int atraso, string tipo)
         {
             int cantFilas = hojaBase.Dimension.Rows;
           
@@ -129,7 +287,7 @@ namespace MultasLectura.LibroPlazos.Controllers
                     object cellValue = hojaBase.Cells[fila, colTipItin].Value;
                     if (cellValue != null)
                     {
-                        if (cellValue.ToString()!.ToLower().Contains("itinerario t1"))
+                        if (cellValue.ToString()!.ToLower().Contains(tipo))
                         {
                             int atrasoBase = int.Parse(hojaBase.Cells[fila, colAtraso].Value.ToString()!);
 
---
+++ Models/TarifaPlazosModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultasLectura.Models
+{
+    public class TarifaPlazosModel
+    {
+        //'a', "días hábiles t1", "itinerario t1"
+
+        private string _tarifa;
+        private string _descripcion;
+        private string _encabezado;
+        private char _letraInicial;
+        private double _baremos;
+
+        public string Tarifa { get { return _tarifa; } set { _tarifa = value; } }
+        public string Descripcion { get { return _descripcion;  } set { _descripcion = value; } }
+        public string Encabezado { get { return _encabezado; } set { _encabezado = value; } }
+        public char LetraInicial { get { return _letraInicial; } 
+            set
+            {
+                _letraInicial = value;
+            } }
+        public double Baremos { get { return _baremos; } set { _baremos = value; } }
+
+            public TarifaPlazosModel(string Tarifa, string Descripcion, string Encabezado, char LetraInicial, double Baremos)
+            {
+                this.Tarifa = Tarifa;
+                this.Descripcion = Descripcion;
+            this.Encabezado = Encabezado;
+            this.LetraInicial = LetraInicial;
+            this.Baremos = Baremos;
+
+            }
+       
+    }
+}
